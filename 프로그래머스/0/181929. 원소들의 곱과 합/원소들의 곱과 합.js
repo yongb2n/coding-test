d172371b854bd7fd7dd78de0554b9826f19f88e8
yongb2n <144973656+yongb2n@users.noreply.github.com>
2024-11-25 20:45:56 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -2,10 +2,10 @@ function solution(num_list) {
     let sum = 0
     let times = 1
     
-    for(let i = 0; i < num_list.length; i++) {
-        sum += num_list[i]
-        times *= num_list[i]
-    }
+   for(let i of num_list) {
+        sum += i
+        times *= i
+   }
     
     return times < sum ** 2 ? 1 : 0
 }
