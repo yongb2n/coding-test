ffc2c1e901f08bae58a08948b871ea927a2bd8a4
yongb2n <144973656+yongb2n@users.noreply.github.com>
2024-11-28 19:52:32 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 2.82 ms, Memory: 35.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
@@ -0,0 +1,11 @@
+function solution(arr, queries) {
+    queries.forEach(([s, e, k], i) => {
+        for(let i = s; i <= e; i++) {
+            if(i % k === 0) {
+                arr[i] += 1
+            }
+        }
+    })
+    
+    return arr
+}
