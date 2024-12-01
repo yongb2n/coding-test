b2438bda478a127addb517d0b2bcba268ecc3a34
yongb2n <144973656+yongb2n@users.noreply.github.com>
2024-12-01 20:08:12 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 5.29 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.js
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.js
@@ -1,10 +1,9 @@
 function solution(number) {
     let result = 0;
-    let arr = [...number];
     
-    for (let i = 0; i < arr.length; i++) {
-        result += Number(arr[i]);
+    for (const i of number) {
+        result += Number(i);
     }
     
-    return result % 9
+    return result % 9;
 }
