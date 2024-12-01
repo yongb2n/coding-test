f6d0157bfed2bb3aca24dce100e1bb6239a45f8d
yongb2n <144973656+yongb2n@users.noreply.github.com>
2024-12-01 20:33:23 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,21 @@
+function solution(s) {
+    
+    let number = {
+      '0': 'zero',
+      '1': 'one',
+      '2': 'two',
+      '3': 'three',
+      '4': 'four',
+      '5': 'five',
+      '6': 'six',
+      '7': 'seven',
+      '8': 'eight',
+      '9': 'nine'
+    };
+
+      for (let key in number) {
+        s = s.replaceAll(number[key], key);
+    }
+    
+    return Number(s);
+}
