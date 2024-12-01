5eada19f1dbdf640fb0dbe70fa2bd36f265f9e3b
yongb2n <kyb2475@gmail.com>
2024-12-01 22:36:57 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -0,0 +1,7 @@
+function solution(s) {
+    const noSpace = s.split(' ').map(Number);
+    const min = Math.min(...noSpace);
+    const max = Math.max(...noSpace);
+    
+    return `${min} ${max}`;
+}
