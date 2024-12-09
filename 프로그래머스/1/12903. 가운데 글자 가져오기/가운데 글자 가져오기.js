04d5df9fa138b4ec5b2656c6e64d528a069fd5aa
yongb2n <kyb2475@gmail.com>
2024-12-09 20:15:58 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,5 +1,7 @@
 function solution(s) {
     const arr = s.split('');
     
-    return arr.length % 2 === 0 ? arr.slice((arr.length / 2) - 1, (arr.length / 2) + 1).join('').toString() : arr.slice(arr.length / 2, arr.length / 2 + 1).toString();
+    return arr.length % 2 === 0 ? 
+        arr.slice((arr.length / 2) - 1, (arr.length / 2) + 1).join('').toString() :
+        arr.slice(arr.length / 2, arr.length / 2 + 1).toString();
 }
